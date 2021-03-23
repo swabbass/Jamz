305cd7788b8f17e130b3c7bc5b71ec77c92ce55e
Ward Abbass <swabbass@gmail.com>
2021-03-23 20:57:59 +0200

    reusable PopupMenuWidget

---
+++ lib/widgets/common/PopupMenuWidget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../StyledTitleText.dart';
+
+class PopupMenu<T> extends StatelessWidget {
+  final List<T> _data = [];
+  late final PopupMenuItemBuilder<T> _builder;
+  late final String _label;
+  late final TextStyle? _style;
+  late final Function(T t, int index)? _onSelected;
+
+  PopupMenu(
+      {String label = "",
+      required List<T> data,
+      required PopupMenuItemBuilder<T> builder,
+      TextStyle? style,
+      Function(T t, int index)? onSelected}) {
+    this._onSelected = onSelected;
+    this._style = style;
+    this._label = label;
+    this._data.addAll(data);
+    this._builder = builder;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<T>(
+        onSelected: (T t) {
+          this._onSelected?.call(t, this._data.indexOf(t));
+        },
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              children: [
+                StyledText(
+                    _label,
+                    this._style != null
+                        ? this._style!
+                        : TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
+                Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.white,
+                )
+              ],
+            ),
+          ),
+        ),
+        itemBuilder: this._builder);
+  }
+}
