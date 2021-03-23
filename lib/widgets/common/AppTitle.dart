6160cd5fbbe85c4b800af608f2aebbb0dff2c5db
Ward Abbass <swabbass@gmail.com>
2021-03-23 20:57:45 +0200

    reusable AppTitle

---
+++ lib/widgets/common/AppTitle.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:progressions/widgets/StyledTitleText.dart';
+import 'package:provider/provider.dart';
+
+class AppTitle extends StatelessWidget {
+  final String? title;
+
+  const AppTitle({
+    Key? key,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: this.title == null
+            ? []
+            : [
+                StyledText(this.title!,
+                    TextStyle(fontWeight: FontWeight.w900, fontSize: 24))
+              ],
+      ),
+    );
+  }
+}
